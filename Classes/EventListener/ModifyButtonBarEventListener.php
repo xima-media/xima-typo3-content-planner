c51eda183cc87c0cfc1be708a1cf17eab9aa63b3
Konrad Michalik <konrad.michalik@xima.de>
2024-07-01 12:57:22 +0200

    feat: add fast content status access to button bar

---
+++ Classes/EventListener/ModifyButtonBarEventListener.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Xima\XimaTypo3ContentPlanner\EventListener;
+
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Template\Components\Buttons\DropDown\DropDownItem;
+use TYPO3\CMS\Backend\Template\Components\ModifyButtonBarEvent;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Xima\XimaTypo3ContentPlanner\Configuration;
+use Xima\XimaTypo3ContentPlanner\Utility\ContentUtility;
+
+final class ModifyButtonBarEventListener
+{
+    public function __construct(private IconFactory $iconFactory, private UriBuilder $uriBuilder)
+    {
+    }
+
+    public function __invoke(ModifyButtonBarEvent $event): void
+    {
+        /** @var ServerRequestInterface $request */
+        $request = $GLOBALS['TYPO3_REQUEST'];
+        $pageId = (int)($request->getParsedBody()['id'] ?? $request->getQueryParams()['id'] ?? 0);
+
+        $page = ContentUtility::getPage($pageId);
+        $buttonBar = $event->getButtonBar();
+
+        $buttons = $event->getButtons();
+        $buttons['right'] ??= [];
+        $dropDownButton = $buttonBar->makeDropDownButton()
+            ->setLabel('Dropdown')
+            ->setTitle($this->getLanguageService()->sL('LLL:EXT:xima_typo3_content_planner/Resources/Private/Language/locallang_be.xlf:status'))
+            ->setIcon($this->iconFactory->getIcon(
+                $page['tx_ximatypo3contentplanner_status'] ? Configuration::STATUS_ICONS[$page['tx_ximatypo3contentplanner_status']] : 'flag-gray'))
+            ;
+
+        foreach (Configuration::STATUS_ICONS as $status => $icon) {
+            $dropDownButton->addItem(
+                GeneralUtility::makeInstance(DropDownItem::class)
+                    ->setLabel($this->getLanguageService()->sL('LLL:EXT:xima_typo3_content_planner/Resources/Private/Language/locallang_be.xlf:status.' . $status))
+                    ->setIcon($this->iconFactory->getIcon(Configuration::STATUS_ICONS[$status]))
+                    ->setHref($this->uriBuilder->buildUriFromRoute(
+                        'tce_db', [
+                            'data' => [
+                                'pages' => [
+                                    $pageId => [
+                                        'tx_ximatypo3contentplanner_status' => $status
+                                    ]
+                                ]
+                            ],
+                            'redirect' => (string)$this->uriBuilder->buildUriFromRoute(
+                                'web_layout',
+                                [
+                                    'id' =>  $pageId
+                                ]
+                            )
+                        ]
+                    )
+                )
+            );
+        }
+        $dropDownButton->addItem(
+            GeneralUtility::makeInstance(DropDownItem::class)
+                ->setLabel($this->getLanguageService()->sL('LLL:EXT:xima_typo3_content_planner/Resources/Private/Language/locallang_be.xlf:reset'))
+                ->setIcon($this->iconFactory->getIcon('actions-close'))
+                ->setHref($this->uriBuilder->buildUriFromRoute(
+                    'tce_db', [
+                        'data' => [
+                            'pages' => [
+                                $pageId => [
+                                    'tx_ximatypo3contentplanner_status' => ''
+                                ]
+                            ]
+                        ],
+                        'redirect' => (string)$this->uriBuilder->buildUriFromRoute(
+                            'web_layout',
+                            [
+                                'id' =>  $pageId
+                            ]
+                        )
+                    ]
+                )
+            )
+        );
+
+
+        $buttons['right'][] = [$dropDownButton];
+        $event->setButtons($buttons);
+    }
+
+    protected function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+}
