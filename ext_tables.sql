5e50d549a90842beb9a009527b1538c70881fd10
Konrad Michalik <konrad.michalik@xima.de>
2024-07-01 09:34:20 +0200

    fix: namespace adjustments regarding tca

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,6 +12,14 @@ CREATE TABLE be_users
 
 CREATE TABLE tx_ximatypo3contentplanner_comment
 (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+
 	foreign_uid   int(11) default '0' not null,
 	foreign_table varchar(255) default '' not null,
 	record_type   varchar(255) default '' not null,
@@ -19,10 +27,20 @@ CREATE TABLE tx_ximatypo3contentplanner_comment
 
 	content       text,
 	author        int(11) DEFAULT NULL,
+	PRIMARY KEY (uid)
 );
 
 CREATE TABLE tx_ximatypo3contentplanner_note
 (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+
 	title   varchar(255) DEFAULT '' NOT NULL,
-	content mediumtext
+	content mediumtext,
+	PRIMARY KEY (uid)
 );
