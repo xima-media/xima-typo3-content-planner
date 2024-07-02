f0e1f34da4060c959225ac0946496d8b601cf592
Konrad Michalik <konrad.michalik@xima.de>
2024-07-02 15:40:14 +0200

    fix: widget naming within preset

--- Configuration/Backend/DashboardPresets.php
+++ Configuration/Backend/DashboardPresets.php
@@ -5,7 +5,7 @@ return [
         'title' => 'LLL:EXT:xima_typo3_content_planner/Resources/Private/Language/locallang.xlf:dashboard.contentPlanner',
         'description' => 'LLL:EXT:xima_typo3_content_planner/Resources/Private/Language/locallang.xlf:dashboard.contentPlanner.description',
         'iconIdentifier' => 'flag-gray',
-        'defaultWidgets' => ['contentStatus-note', 'contentStatus-overview', 'contentStatus-current', 'contentStatus-update', 'contentStatus-danger', 'contentStatus-info', 'contentStatus-warning', 'contentStatus-success', 'contentStatus-comment'],
+        'defaultWidgets' => ['contentStatus-notes', 'contentStatus-overview', 'contentStatus-current', 'contentStatus-update', 'contentStatus-danger', 'contentStatus-info', 'contentStatus-warning', 'contentStatus-success', 'contentStatus-comment'],
         'showInWizard' => true,
     ],
 ];
