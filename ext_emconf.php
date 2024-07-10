2f42cdbd50cb507089692df71b14616623f39d24
Konrad Michalik <konrad.michalik@xima.de>
2024-07-10 10:55:17 +0200

    feat: add additional technical extension information

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var string $_EXTKEY */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Content Planner',
+    'description' => 'This extension provides a page status functionality to support the planning of content work.',
+    'category' => 'module',
+    'author' => 'Konrad Michalik',
+    'author_email' => 'konrad.michalik@xima.de',
+    'author_company' => 'XIMA MEDIA GmbH',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.1.0-8.99.99',
+            'typo3' => '12.0.0-12.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
