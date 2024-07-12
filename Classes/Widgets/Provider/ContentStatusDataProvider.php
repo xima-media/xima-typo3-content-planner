3eb4c5d11beab4f416406756e9c30860fed027b3
Konrad Michalik <konrad.michalik@xima.de>
2024-07-12 15:28:52 +0200

    cleanup: unnecessary code

--- Classes/Widgets/Provider/ContentStatusDataProvider.php
+++ Classes/Widgets/Provider/ContentStatusDataProvider.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace Xima\XimaTypo3ContentPlanner\Widgets\Provider;
 
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Dashboard\Widgets\ListDataProviderInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
-use Xima\XimaTypo3ContentPlanner\Domain\Model\Dto\StatusItem;
 use Xima\XimaTypo3ContentPlanner\Domain\Repository\StatusRepository;
 use Xima\XimaTypo3ContentPlanner\Utility\ContentUtility;
-use Xima\XimaTypo3ContentPlanner\Utility\ExtensionUtility;
 
 class ContentStatusDataProvider implements ListDataProviderInterface
 {
@@ -24,45 +20,7 @@ class ContentStatusDataProvider implements ListDataProviderInterface
      */
     public function getItems(): array
     {
-        return $this->getItemsByDemand();
-    }
-
-    /**
-     * @throws \Doctrine\DBAL\Exception
-     */
-    public function getItemsByDemand(?int $status = null, ?int $assignee = null, int $maxResults = 20): array
-    {
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
-
-        $additionalWhere = '';
-        $additionalParams = [
-            'limit' => $maxResults,
-        ];
-        if ($status) {
-            $additionalWhere .= ' AND tx_ximatypo3contentplanner_status = :status';
-            $additionalParams['status'] = $status;
-        }
-        if ($assignee) {
-            $additionalWhere .= ' AND tx_ximatypo3contentplanner_assignee = :assignee';
-            $additionalParams['assignee'] = $assignee;
-        }
-        $sqlArray = [];
-        foreach (ExtensionUtility::getRecordTables() as $table) {
-            $sqlArray[] = '(SELECT "' . $table . '" as tablename, uid, title, tstamp, tx_ximatypo3contentplanner_status, tx_ximatypo3contentplanner_assignee, tx_ximatypo3contentplanner_comments FROM ' . $table . ' WHERE tx_ximatypo3contentplanner_status IS NOT NULL' . $additionalWhere . ')';
-        }
-        $sql = implode(' UNION ', $sqlArray) . ' ORDER BY tstamp DESC LIMIT :limit';
-
-        $statement = $queryBuilder->getConnection()->executeQuery($sql, $additionalParams);
-        $results = $statement->fetchAllAssociative();
-
-        foreach ($results as $result) {
-            try {
-                $items[] = StatusItem::create($result);
-            } catch (\Exception $e) {
-            }
-        }
-
-        return $items;
+        return [];
     }
 
     public function getStatus(): QueryResultInterface|array
