9523751f6403d38c39a61999247738b31003f278
Konrad Michalik <konrad.michalik@xima.de>
2024-07-15 17:32:36 +0200

    fix: adjust permission check

--- Classes/Utility/PermissionUtility.php
+++ Classes/Utility/PermissionUtility.php
@@ -4,18 +4,26 @@ declare(strict_types=1);
 
 namespace Xima\XimaTypo3ContentPlanner\Utility;
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Type\Bitmask\Permission;
+
 class PermissionUtility
 {
     public static function checkAccessForRecord(string $tablename, $record): bool
     {
         $backendUser = $GLOBALS['BE_USER'];
         /* @var $backendUser \TYPO3\CMS\Core\Authentication\BackendUserAuthentication */
-
-        // ToDo: seems a bit ugly, but it works (for now)
-        if ($tablename === 'pages' && !$backendUser->doesUserHaveAccess($record, 1)) {
+        if ($tablename === 'pages' && !BackendUtility::readPageAccess(
+                $record['uid'],
+                $GLOBALS['BE_USER']->getPagePermsClause(Permission::PAGE_SHOW)
+            )) {
             return false;
         }
-        if (!$backendUser->check('tables_modify', $tablename)) {
+
+        if (!$backendUser->check('tables_select', $tablename) || !BackendUtility::readPageAccess(
+                $record['pid'],
+                $GLOBALS['BE_USER']->getPagePermsClause(Permission::PAGE_SHOW)
+            )) {
             return false;
         }
         return true;
