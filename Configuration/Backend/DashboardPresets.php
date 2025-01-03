ca1043cfe70fc8ee69b2c8062e4f9af82afb019e
Konrad Michalik <konrad.michalik@xima.de>
2025-01-03 15:24:41 +0100

    feat: rearrange dashboard widgets

--- Configuration/Backend/DashboardPresets.php
+++ Configuration/Backend/DashboardPresets.php
@@ -5,7 +5,7 @@ return [
         'title' => 'LLL:EXT:xima_typo3_content_planner/Resources/Private/Language/locallang.xlf:dashboard.contentPlanner',
         'description' => 'LLL:EXT:xima_typo3_content_planner/Resources/Private/Language/locallang.xlf:dashboard.contentPlanner.description',
         'iconIdentifier' => 'flag-gray',
-        'defaultWidgets' => ['contentPlanner-overview', 'contentPlanner-current', 'contentPlanner-update', 'contentPlanner-status', 'contentPlanner-comment'],
+        'defaultWidgets' => ['contentPlanner-overview', 'contentPlanner-update', 'contentPlanner-current', 'contentPlanner-comment', 'contentPlanner-status'],
         'showInWizard' => true,
     ],
 ];
