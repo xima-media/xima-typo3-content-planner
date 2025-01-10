dad86855a5108118c36005c7d6dd3778a70db851
Konrad Michalik <konrad.michalik@xima.de>
2025-01-10 14:12:31 +0100

    fix: remove old toolbar registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,8 +10,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Controller\Page
     'className' => \Xima\XimaTypo3ContentPlanner\Controller\TreeController::class,
 ];
 
-$GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1719820170] = \Xima\XimaTypo3ContentPlanner\Backend\ToolbarItems\UpdateItem::class;
-
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['ximatypo3contentplanner_statuscache'] ??= [];
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['ximatypo3contentplanner_statuscache'] = \Xima\XimaTypo3ContentPlanner\Hooks\DataHandlerHook::class . '->clearCachePostProc';
 
