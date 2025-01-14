f400d6d22862cd4ff82e1f5c8cd4a8ad06588cc5
Konrad Michalik <konrad.michalik@gmx.de>
2025-01-14 19:45:34 +0100

    fix: add status header for list module

--- Classes/Middleware/BackendContentModifierMiddleware.php
+++ Classes/Middleware/BackendContentModifierMiddleware.php
@@ -12,13 +12,14 @@ use TYPO3\CMS\Core\Core\RequestId;
 use TYPO3\CMS\Core\Http\Response;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 use Xima\XimaTypo3ContentPlanner\Configuration;
+use Xima\XimaTypo3ContentPlanner\Domain\Model\Status;
 use Xima\XimaTypo3ContentPlanner\Domain\Repository\BackendUserRepository;
 use Xima\XimaTypo3ContentPlanner\Domain\Repository\CommentRepository;
 use Xima\XimaTypo3ContentPlanner\Domain\Repository\RecordRepository;
 use Xima\XimaTypo3ContentPlanner\Domain\Repository\StatusRepository;
 use Xima\XimaTypo3ContentPlanner\Utility\ExtensionUtility;
-use Xima\XimaTypo3ContentPlanner\Utility\UrlHelper;
 
 class BackendContentModifierMiddleware implements MiddlewareInterface
 {
@@ -33,80 +34,98 @@ class BackendContentModifierMiddleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (UrlHelper::isRelevantRecordEditRequest($request)) {
-            $response = $handler->handle($request);
-            $content = $response->getBody()->__toString();
-
-            if ($content !== '') {
-                $table = array_key_first($request->getQueryParams()['edit']);
-                $uid = $request->getQueryParams()['edit'][$table] ?? 0;
-                $uid = is_array($uid) ? (int)array_key_first($uid) : (int)$uid;
-
-                $newContent = $this->addRecordEditHeader($table, $uid, $content);
-                if (!$newContent) {
-                    return $response;
-                }
-
-                $newResponse = new Response();
-                $newResponse->getBody()->write($newContent);
-
-                return $newResponse;
-            }
-            return $response;
+        if ($this->isRelevantRecordEditRequest($request)) {
+            return $this->handleRecordEditRequest($request, $handler);
         }
 
-        if (UrlHelper::isRelevantWebLayoutRequest($request)) {
-            $response = $handler->handle($request);
+        if ($this->isRelevantWebListRequest($request)) {
+            return $this->handleListLayoutRequest($request, $handler);
+        }
+
+        if ($this->isRelevantWebLayoutRequest($request)) {
+            return $this->handleWebLayoutRequest($request, $handler);
+        }
+
+        return $handler->handle($request);
+    }
+
+    private function handleWebLayoutRequest(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
             $content = $response->getBody()->__toString();
 
             $pid = $request->getQueryParams()['id'] ?? 0;
             if (!$pid) {
-                return $handler->handle($request);
+                return $response;
             }
 
             $newResponse = new Response();
             $newResponse->getBody()->write($content . $this->addStatusHintToContentElement((int)$pid));
 
             return $newResponse;
-        }
-
-        return $handler->handle($request);
     }
 
-    private function addRecordEditHeader(string $table, int $uid, string $content): string|bool
-    {
-        $record = $this->recordRepository->findByUid($table, $uid);
+    private function handleRecordEditRequest(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+        $response = $handler->handle($request);
+        $content = $response->getBody()->__toString();
 
-        if ($record) {
-            $status = $this->statusRepository->findByUid($record['tx_ximatypo3contentplanner_status']);
+        if ($content !== '') {
+            $table = array_key_first($request->getQueryParams()['edit']);
+            $uid = $request->getQueryParams()['edit'][$table] ?? 0;
+            $uid = is_array($uid) ? (int)array_key_first($uid) : (int)$uid;
 
-            if (!$status) {
-                return false;
+            $additionalContent = $this->generateStatusHeader($table, $uid, $content);
+            if (!$additionalContent) {
+                return $response;
             }
-
-            $view = GeneralUtility::makeInstance(StandaloneView::class);
-            $view->setTemplatePathAndFilename('EXT:' . Configuration::EXT_KEY . '/Resources/Private/Templates/Backend/Header/HeaderInfo.html');
-
-            $view->assignMultiple([
-                'mode' => 'edit',
-                'data' => $record,
-                'assignee' => $this->backendUserRepository->getUsernameByUid((int)$record['tx_ximatypo3contentplanner_assignee']),
-                'icon' => $status->getColoredIcon(),
-                'status' => $status,
-                'comments' => $record['tx_ximatypo3contentplanner_comments'] ? $this->commentRepository->findAllByRecord($uid, $table) : [],
-                'pid' => $record['pid'],
-                'userid' => $GLOBALS['BE_USER']->user['uid'],
-            ]);
-
-            $additionalContent = $view->render();
-            $additionalContent .= ExtensionUtility::getCssTag('EXT:' . Configuration::EXT_KEY . '/Resources/Public/Css/Header.css', ['nonce' => $this->requestId->nonce]);
 
             /*
             * This is a workaround to add the header content to the top of the record edit form.
             */
-            return preg_replace('/(<div\s+class="typo3-TCEforms")/', $additionalContent . '$1', $content);
+            $newContent = preg_replace(
+                '/(<div\s+class="typo3-TCEforms")/',
+                $additionalContent . '$1',
+                $content
+            );
+
+            $newResponse = new Response();
+            $newResponse->getBody()->write($newContent);
+
+            return $newResponse;
         }
-        return false;
+        return $response;
+    }
+
+    private function handleListLayoutRequest(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+        $content = $response->getBody()->__toString();
+
+        if ($content !== '') {
+
+            $uid = (int)$request->getQueryParams()['id'] ?? 0;
+
+            $additionalContent = $this->generateStatusHeader('pages', $uid, $content);
+
+            if (!$additionalContent) {
+                return $response;
+            }
+
+            /*
+            * This is a workaround to add the header content to the top of the list module.
+            */
+            $newContent = preg_replace(
+                '/(<typo3-backend-editable-page-title\b[^>]*>.*?<\/typo3-backend-editable-page-title>)/is',
+                '$1' . $additionalContent,
+                $content
+            );
+
+            $newResponse = new Response();
+            $newResponse->getBody()->write($newContent);
+
+            return $newResponse;
+        }
+        return $response;
     }
 
     private function addStatusHintToContentElement(int $pid): string
@@ -124,5 +143,73 @@ class BackendContentModifierMiddleware implements MiddlewareInterface
         }
 
         return '<style>' . implode(' ', $styling) . '</style>';
+    }
+
+
+    private function generateStatusHeader(string $table, int $uid): string|bool
+    {
+        $record = $this->recordRepository->findByUid($table, $uid);
+
+        if ($record) {
+            $status = $this->statusRepository->findByUid($record['tx_ximatypo3contentplanner_status']);
+
+            if (!$status) {
+                return false;
+            }
+
+            $additionalContent = $this->renderStatusHeaderContentView(
+                $record,
+                $status,
+                $record['tx_ximatypo3contentplanner_comments'] ? $this->commentRepository->findAllByRecord($uid, $table) : []
+            );
+
+            return $additionalContent;
+        }
+        return false;
+    }
+
+    private function renderStatusHeaderContentView(array $record, Status $status, array $comments): string{
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplatePathAndFilename('EXT:' . Configuration::EXT_KEY . '/Resources/Private/Templates/Backend/Header/HeaderInfo.html');
+
+        $view->assignMultiple([
+            'mode' => 'edit',
+            'data' => $record,
+            'assignee' => $this->backendUserRepository->getUsernameByUid((int)$record['tx_ximatypo3contentplanner_assignee']),
+            'icon' => $status->getColoredIcon(),
+            'status' => $status,
+            'comments' => $comments,
+            'pid' => $record['pid'],
+            'userid' => $GLOBALS['BE_USER']->user['uid'],
+        ]);
+
+        $content = $view->render();
+        $content .= ExtensionUtility::getCssTag('EXT:' . Configuration::EXT_KEY . '/Resources/Public/Css/Header.css', ['nonce' => $this->requestId->nonce]);
+
+        return $content;
+    }
+
+    private function isRelevantWebLayoutRequest(ServerRequestInterface $request): bool
+    {
+        return $request->getAttribute('applicationType') === SystemEnvironmentBuilder::REQUESTTYPE_BE
+            && $request->getAttribute('module') !== null
+            && $request->getAttribute('module')->getIdentifier() === 'web_layout'
+            && in_array('tt_content', $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS'][Configuration::EXT_KEY]['registerAdditionalRecordTables']);
+    }
+
+    private function isRelevantRecordEditRequest(ServerRequestInterface $request): bool
+    {
+        return $request->getAttribute('applicationType') === SystemEnvironmentBuilder::REQUESTTYPE_BE
+            && ExtensionUtility::isFeatureEnabled(Configuration::FEATURE_RECORD_EDIT_HEADER_INFO)
+            && array_key_exists('edit', $request->getQueryParams())
+            && (array_key_first($request->getQueryParams()['edit']) === 'pages' || in_array(array_key_first($request->getQueryParams()['edit']), $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS'][Configuration::EXT_KEY]['registerAdditionalRecordTables']));
+    }
+
+    private function isRelevantWebListRequest(ServerRequestInterface $request): bool
+    {
+        return $request->getAttribute('applicationType') === SystemEnvironmentBuilder::REQUESTTYPE_BE
+            && ExtensionUtility::isFeatureEnabled(Configuration::FEATURE_RECORD_EDIT_HEADER_INFO)
+            && $request->getAttribute('module') !== null
+            && $request->getAttribute('module')->getIdentifier() === 'web_list';
     }
 }
