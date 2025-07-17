b83052977ee309b4d957ed3c4e005349e5bcec68
Konrad Michalik <km@move-elevator.de>
2025-07-17 10:25:30 +0200

    feat: add file headers to all PHP files for licensing and attribution

--- Tests/.typo3-setup/packages/sitepackage/ext_localconf.php
+++ Tests/.typo3-setup/packages/sitepackage/ext_localconf.php
@@ -2,4 +2,23 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the TYPO3 CMS extension "xima_typo3_content_planner".
+ *
+ * Copyright (C) 2024-2025 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['xima_typo3_content_planner']['registerAdditionalRecordTables'] = ['sys_category', 'tt_content'];
--- php-cs-fixer.php
+++ php-cs-fixer.php
@@ -2,17 +2,44 @@
 
 declare(strict_types=1);
 
-$finder = (new PhpCsFixer\Finder())
-    ->ignoreVCSIgnored(true)
-    ->in(__DIR__);
+/*
+ * This file is part of the TYPO3 CMS extension "xima_typo3_content_planner".
+ *
+ * Copyright (C) 2024-2025 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
-return (new PhpCsFixer\Config())
-    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
-    ->setRiskyAllowed(true)
-    ->setUnsupportedPhpVersionAllowed(true)
-    ->setRules([
-        '@PSR12' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'trailing_comma_in_multiline' => true,
-    ])
-    ->setFinder($finder);
+use EliasHaeussler\PhpCsFixerConfig;
+use TYPO3\CodingStandards;
+
+$header = PhpCsFixerConfig\Rules\Header::create(
+    \Xima\XimaTypo3ContentPlanner\Configuration::EXT_KEY,
+    PhpCsFixerConfig\Package\Type::TYPO3Extension,
+    PhpCsFixerConfig\Package\Author::create('Konrad Michalik', 'hej@konradmichalik.dev'),
+    PhpCsFixerConfig\Package\CopyrightRange::from(2024),
+    PhpCsFixerConfig\Package\License::GPL2OrLater,
+);
+
+$config = CodingStandards\CsFixerConfig::create();
+$finder = $config->getFinder()
+    ->in(__DIR__)
+    ->ignoreVCSIgnored(true)
+    ->ignoreDotFiles(false)
+;
+
+return PhpCsFixerConfig\Config::create()
+    ->withConfig($config)
+    ->withRule($header)
+;
