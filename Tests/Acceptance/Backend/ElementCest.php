fc0c9080320572c459296451336bb84a789a9607
Konrad Michalik <konrad.michalik@gmx.de>
2025-08-25 16:00:55 +0200

    chore: add license comments to PHP files and update composer dependencies

--- Tests/Acceptance/Backend/ElementCest.php
+++ Tests/Acceptance/Backend/ElementCest.php
@@ -1,11 +1,28 @@
 <?php
 
+/*
+ * This file is part of the TYPO3 CMS extension "xima_typo3_content_planner".
+ *
+ * Copyright (C) 2024-2025 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace Xima\XimaTypo3ContentPlanner\Tests\Acceptance\Backend;
 
 use Xima\XimaTypo3ContentPlanner\Tests\Acceptance\Support\AcceptanceTester;
-use Xima\XimaTypo3ContentPlanner\Tests\Acceptance\Support\Enums\Selectors;
 use Xima\XimaTypo3ContentPlanner\Tests\Acceptance\Support\Helper\PageTree;
-use Xima\XimaTypo3ContentPlanner\Tests\Acceptance\Support\Helper\ShadowDom;
 
 class ElementCest
 {
@@ -17,8 +34,7 @@ class ElementCest
     public function createElementAndSave(
         AcceptanceTester $I,
         PageTree $pageTree,
-    ): void
-    {
+    ): void {
         $I->click('Page');
         $I->waitForElementVisible(PageTree::$treeSelector);
         $I->wait(2);
