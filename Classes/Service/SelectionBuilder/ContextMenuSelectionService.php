89fefd8039a61f385990914163410e76a77e7a48
Konrad Michalik <km@move-elevator.de>
2026-01-09 13:32:58 +0100

    fix: update copyright year to 2026 in multiple files

--- Classes/Service/SelectionBuilder/ContextMenuSelectionService.php
+++ Classes/Service/SelectionBuilder/ContextMenuSelectionService.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the "xima_typo3_content_planner" TYPO3 CMS extension.
  *
- * (c) 2024-2025 Konrad Michalik <hej@konradmichalik.dev>
+ * (c) 2024-2026 Konrad Michalik <hej@konradmichalik.dev>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
